cfa309ad5e96024116fb54220f62b7e2c9e68aa6
Jhosep <jose-zromero@hotmail.com>
2023-02-28 13:07:56 -0600

    Add babel file

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
   entry: {
     application: "./app/javascript/application.js"
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
